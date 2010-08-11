037d43a6b786af9d88f63100979bdb06f4585527
Max Williams <max@new-bamboo.co.uk>
2010-08-11 17:40:29 +0100

    some tweaks for latest version of Pusher presence

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,14 +1,13 @@
 class PusherController < ApplicationController
   def auth
-    auth = Pusher[params[:channel_name]].socket_auth(params[:socket_id])
     identity = Identity.find_by_session_id(session[:session_id])
-    render :json => { 
-            :auth => auth, 
-            :user_id => session[:session_id], 
-            :user_info => identity ? {
-              :email =>  identity.email,
-              :gravatar => identity.gravatar
-            } : {}
-    }
+    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
+      :user_id => current_user.id,
+      :user_info => identity ? {
+        :email =>  identity.email,
+        :gravatar => identity.gravatar
+      } : {}
+    })
+		render :json => response
   end
 end
