ae69d9cd8a01a6a89fe36b5adbf1b8c6caf132a7
Max Williams <max@new-bamboo.co.uk>
2010-08-11 17:48:18 +0100

    I'm a dolt - copy and paste fail

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,7 +2,7 @@ class PusherController < ApplicationController
   def auth
     identity = Identity.find_by_session_id(session[:session_id])
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
-      :user_id => current_user.id,
+      :user_id => session[:session_id],
       :user_info => identity ? {
         :email =>  identity.email,
         :gravatar => identity.gravatar
