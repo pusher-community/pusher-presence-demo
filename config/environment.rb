ca5a7d1d18d27c7933746eae9b1c0beefbd0e869
Martyn Loughran <me@mloughran.com>
2011-01-25 16:32:07 +0000

    Use the pusher addon :)

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 PusherPresenceDemo::Application.initialize!
 
-Pusher.app_id = ENV['PUSHER_APP_ID']
-Pusher.key = ENV['PUSHER_KEY']
-Pusher.secret = ENV['PUSHER_SECRET']
-
 # not strictly necessary, but referenced in views
 PUSHER_JS_DOMAIN = 'http://js.pusherapp.com'
 PUSHER_SOCKET_HOST = 'ws.pusherapp.com'
