5e550772def739fffd5e1880e02593774f259517
Martyn Loughran <me@mloughran.com>
2011-01-25 16:41:25 +0000

    Make Pusher API requests using trigger_async on heroku

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '~> 3.0.0'
 gem 'pusher'
+gem 'em-http-request' # Required by trigger_async
 gem 'gravatar-ultimate'
 
 group :development do
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,18 @@
 class MessagesController < ApplicationController
   def create
-    Pusher['presence-demo'].trigger('message', {
-      :user_id => session[:session_id],
-      :text => params[:text]
-    })
+    if EM.reactor_running?
+      # Heroku apps run inside eventmachine so we can make API calls to pusher
+      # outside the request-response cycle
+      Pusher['presence-demo'].trigger_async('message', {
+        :user_id => session[:session_id],
+        :text => params[:text]
+      })
+    else
+      Pusher['presence-demo'].trigger('message', {
+        :user_id => session[:session_id],
+        :text => params[:text]
+      })
+    end
     head :ok
   end
 end
