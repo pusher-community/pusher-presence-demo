90e6bb9c6ce38c5f9a8e8adf7a1dd6df3b1e8f4d
Micheil Smith <micheil@brandedcode.com>
2011-10-28 18:40:38 +0100

    Added test route as to allow very easy testing of local javascript against the presence demo.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,11 @@
 class DemoController < ApplicationController
   def index
     @identity = session[:identity]
+    @use_local_js = false
+  end
+
+  def test_index
+    @identity = session[:identity]
+    @use_local_js = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ PusherPresenceDemo::Application.routes.draw do
   resource :identity
   resources :messages
   match '/' => 'demo#index'
+  match '/test' => 'demo#test_index'
   match '/pusher/auth' => 'pusher#auth'
 end
